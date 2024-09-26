a590c7519c6ed3b1b312361a5965b968021d115a
AjayParashar2009 <149396366+AjayParashar2009@users.noreply.github.com>
2024-09-26 23:47:24 +0530

    commit

---
+++ music player/script.js
@@ -0,0 +1,37 @@
+let song = document.getElementById('song')
+let contIcon = document.getElementById('contIcon')
+let progress = document.getElementById('progress')
+
+
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+
+}
+
+
+function playPause() {
+    if (contIcon.classList.contains('fa-pause')) {
+        song.pause()
+        contIcon.classList.remove('fa-pause')
+        contIcon.classList.add('fa-play')
+    } else {
+        song.play()
+        contIcon.classList.remove('fa-play')
+        contIcon.classList.add('fa-pause')
+    }
+}
+
+if(song.play()){ 
+setInterval(() => {
+    // progress.value = song.currentTime;
+}, 500)
+}
+
+
+progress.onchange = function () {
+    song.play()
+    song.currentTime = progress.value
+    contIcon.classList.remove('fa-play')
+    contIcon.classList.add('fa-pause')
+}
